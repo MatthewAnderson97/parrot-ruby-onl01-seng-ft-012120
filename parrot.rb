b6f1c285ce77d66824c71e46aabbee99b5cd9918
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-23 08:03:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot()
-  print "Squawk!"
- return "Squawk!"
+def parrot(name = "Squawk!")
+  name = "Pretty bird"
+  name
 end
